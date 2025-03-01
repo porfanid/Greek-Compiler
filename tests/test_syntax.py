1fb0d766d86bdb619c04cb84856d372f0b76f1a6
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2025-03-01 02:07:31 +0200

    Deleted unused files and updated the syntax tests

--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -11,9 +11,9 @@ from src.compiler import perform_syntax_analysis
 class TestSyntax(unittest.TestCase):
     def test_parser_correct(self):
         file =  "./tests/syntax_inputs/correct.gr"
-        perform_syntax_analysis(file, False)
+        perform_syntax_analysis(file, True)
 
     def test_parser_false(self):
         file = "./tests/syntax_inputs/false.gr"
         with self.assertRaises(SyntaxError):
-            perform_syntax_analysis(file, False)
+            perform_syntax_analysis(file, True)
